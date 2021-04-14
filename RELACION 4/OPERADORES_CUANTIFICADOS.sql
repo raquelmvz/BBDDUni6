af9dcca5c75e09cedaddfcbee8e0d7ca93d9d697
raquelmvz <raquel.mvz98@gmail.com>
2021-04-14 12:03:28 +0200

    fin ejemplos

--- RELACION 4/OPERADORES_CUANTIFICADOS.sql
+++ RELACION 4/OPERADORES_CUANTIFICADOS.sql
@@ -1,8 +1,19 @@
 /* Busca empleados con el mismo nombre que alguno del departamento 122 */
+select nomem, numde
+from empleados
+where nomem = ANY (select nomem from empleados where numde = '122')
+	and numde <> 122;
 
 /* Busca empleados cuyo salario sea superior a todos los del departamento 122 */
+select concat_ws(' ', nomem, ape1em, ifnull(ape2em,'')) as nombre
+from empleados
+where salarem > ALL (select salarem from empleados where numde = '122');
+
 
 /* Busca empleados cuya extension telefonica sea diferente a los del depto 122 */
+select concat_ws(' ', nomem, ape1em, ifnull(ape2em,'')) as nombre
+from empleados
+where extelem NOT IN (select extelem from empleados where numde = '122');
 
 /*** OPERADORES CUANTIFICADORES SON:
 
@@ -33,3 +44,9 @@ EQUIVALENCIAS EN EL USO DE LOS OPERADORES CUANTIFICADOS ==>
 expresion1 = SOME (subquery)
 expresion1 = ANY (subquery)
 expresion1 IN (subquery)
+
+OTRA EQUIVALENCIA ==> 
+
+expresion1 <> ALL (subquery)
+expresion1 NOT IN (subquery)
+
