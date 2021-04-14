c0b3590df439c4e0c6a0882c603bb2f7181b9487
raquelmvz <raquel.mvz98@gmail.com>
2021-04-14 10:35:48 +0200

    Crea archivo

---
+++ RELACION 4/subselects.sql
@@ -0,0 +1,23 @@
+/* 22 - Para los departamentos cuyo salario medio supera al de la empresa, 
+hallar cuantas extensiones telefónicas tienen */
+
+
+/* 30 - Obtener por orden alfabético los nombres de los empleados cuyo salario 
+igualen o superen al de Claudia Fierro en más de
+un 50%. */
+
+/* 34 - Obtener por orden alfabético los nombres de los empleados 
+que trabajan en el mismo departamento que Pilar Gálvez o
+Dorotea Flor. */
+
+/* 35 - Obtener por orden alfabético los nombres de los empleados 
+cuyo salario supere en tres veces y media o más al mínimo
+salario de los empleados del departamento 122. */
+
+/* 36 - Hallar el salario máximo y mínimo para cada grupo de 
+empleados con igual número de hijos y que tienen al
+menos uno, y solo si hay más de un empleado en el grupo. */
+
+/* 28 - Crear una vista en la que aparezcan todos los datos de 
+los empleados que cumplen 65 años de edad este año. */
+
